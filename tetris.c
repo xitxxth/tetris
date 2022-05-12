6ecccbfbe47ae4376829e47c26b0b56c6e3a0fb2
xitxxth <jongyn1128@gmail.com>
2022-05-12 15:20:42 +0900

    rec

--- tetris.c
+++ tetris.c
@@ -704,9 +704,12 @@ for(rotate=0; rotate<4; rotate++){
 for(i=0; i<HEIGHT; i++)
 	for(j=0; j<WIDTH; j++)
 		curr->recField[i][j] = originField[i][j];
-		y=0;
+		y=-1;
 		while(CheckToMove(originField, curr->curBlockID, rotate, ++y, x)==1);	y--;
-		if(CheckToMove(originField, curr->curBlockID, rotate, y, x)==0)	continue;
+		if(CheckToMove(originField, curr->curBlockID, rotate, y, x)==0){
+		if(y==-1)	gameOver=1;
+		}
+		else	continue;
 		curr->accumulatedScore = acc_score + AddBlockToField(curr->recField, curr->curBlockID, rotate, y, x);
 		curr->accumulatedScore += DeleteLine(curr->recField);
 		if(curr->level<VISIBLE_BLOCKS-1)	curr->accumulatedScore+=recommend(curr);
